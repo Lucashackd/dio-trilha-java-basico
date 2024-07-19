8ece2fec6a88281cfabe97a5bf528995dc1b7970
Lucas Hackbart Döhnert <lucashackd@gmail.com>
2024-07-19 15:52:52 -0300

    finish

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
+import java.util.Locale;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Conta !");
+        int numero = sc.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = sc.next();
+
+        System.out.println("Por favor, digite o nome do Cliente !");
+        String nomeCliente = sc.next();
+
+        System.out.println("Por favor, digite o saldo do Cliente !");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta " +
+                "em nosso banco, sua agência é " + agencia + ", conta " + numero +
+                " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
